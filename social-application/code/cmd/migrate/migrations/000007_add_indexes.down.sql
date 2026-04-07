d08d8178a804bda49b644f04fb0e039599d3796a
Swaniket Chowdhury <swaniketchowdhury@gmail.com>
2026-04-07 22:22:00 +0530

    Social | Added indexes via migration

---
+++ social-application/code/cmd/migrate/migrations/000007_add_indexes.down.sql
@@ -0,0 +1,8 @@
+DROP INDEX IS EXISTS idx_comments_content;
+
+DROP INDEX IS EXISTS idx_posts_title;
+DROP INDEX IS EXISTS idx_posts_tags;
+
+DROP INDEX IS EXISTS idx_users_username;
+DROP INDEX IS EXISTS idx_posts_user_id;
+DROP INDEX IS EXISTS idx_comments_post_id;
